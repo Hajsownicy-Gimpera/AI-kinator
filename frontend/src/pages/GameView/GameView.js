457b70594d7023da712f4ddcd013c6ed82de77b2
smadziara <smadziara@student.agh.edu.pl>
2026-04-25 13:48:11 +0200

    FE-BUGS-1: fix for unused in GameView

--- frontend/src/pages/GameView/GameView.js
+++ frontend/src/pages/GameView/GameView.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useParams} from 'react-router-dom';
 
 const GameView = () => {
   const { roomId } = useParams();
