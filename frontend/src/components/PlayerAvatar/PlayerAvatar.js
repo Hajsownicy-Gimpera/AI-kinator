aca35b3bbd50b3cee808de741b926e47379e8def
Micro9261 <140260920+Micro9261@users.noreply.github.com>
2026-05-18 16:16:44 +0200

    FE-7: Avatar file and printScript

--- frontend/src/components/PlayerAvatar/PlayerAvatar.js
+++ frontend/src/components/PlayerAvatar/PlayerAvatar.js
@@ -13,18 +13,18 @@ import styled from 'styled-components';
 const AvatarContainer = styled.div`
   width: ${(props) => props.$size || '120px'};
   height: ${(props) => props.$size || '120px'};
-  background-image: url('/assets/Avatars.jpg');
+  background-image: url('/assets/Avatars_background_free.png');
   background-size: 300% 200%; /* 3 columns, 2 rows */
   background-color: transparent;
   background-repeat: no-repeat;
   background-position: ${(props) => {
     const positions = [
       '0% 0%',      // 0: top-left
-      '33.33% 0%',  // 1: top-center
-      '66.66% 0%',  // 2: top-right
-      '0% 50%',     // 3: bottom-left
-      '33.33% 50%', // 4: bottom-center
-      '66.66% 50%', // 5: bottom-right
+      '48% 0%',  // 1: top-center
+      '96% 0%',  // 2: top-right
+      '0% 103%',     // 3: bottom-left
+      '42% 103%', // 4: bottom-center
+      '96% 103%', // 5: bottom-right
     ];
     return positions[props.$avatarIndex || 0];
   }};
